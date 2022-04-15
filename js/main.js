8c13533e53a50647faf222cdbe8e9f5fc5ecf0b7
Dorian Brown <brown.3794@gmail.com>
2022-04-14 21:02:49 -0400

    feat: :tada: Initial Push
    Initial push of local repo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-//Example fetch using pokemonapi.co
-document.getElementById("getWord").addEventListener("click", getFetch);
+getWord();
+document.getElementById("getWord").addEventListener("click", getWord);
+// document.querySelector("#guess").addEventListener("click", checkGuess(word));
 
-function getFetch() {
+function getWord() {
   const url = "https://palabras-aleatorias-public-api.herokuapp.com/random";
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
@@ -10,9 +11,9 @@ function getFetch() {
       makeWordPuzzle(word);
       console.log(word);
 
-      document.querySelector(".hint").innerText = decodeURI(
-        data.body.DefinitionMD
-      );
+      // document.querySelector(".hint").innerText = decodeURI(
+      //   data.body.DefinitionMD
+      // );
     })
     .catch((err) => {
       console.log(`error ${err}`);
@@ -22,11 +23,34 @@ function getFetch() {
 //const makeHint = (word) => {};
 
 const makeWordPuzzle = (word) => {
+  clearLetters();
   wordArr = [...word].map((letter) => letter.toUpperCase());
   wordArr.forEach((letter) => {
     let letterDiv = document.createElement("span");
-    letterDiv.classList.add("letter");
+    letterDiv.classList.add("letter", "clay");
     letterDiv.innerHTML = letter;
     document.querySelector("#wordPuzzle").appendChild(letterDiv);
+    hideLetters();
   });
+};
+
+const clearLetters = () => {
+  let letterList = document.querySelector("#wordPuzzle");
+  while (letterList.firstChild) {
+    letterList.removeChild(letterList.lastChild);
+  }
+};
+
+const hideLetters = () => {
+  let letterList = document.querySelectorAll(".letter");
+  Array.from(letterList)
+    .slice(1, -1)
+    .forEach((letter) => {
+      letter.classList.add("hide");
+    });
+};
+
+const checkGuess = (word) => {
+  let guess = document.querySelector("#guess").value.toLowerCase();
+  word === guess ? console.log("correct") : console.log("incorrect");
 };
